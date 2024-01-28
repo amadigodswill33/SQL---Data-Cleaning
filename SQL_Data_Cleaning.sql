c05391cac8f63eecfafa17d13b9e65b92791a36e
Godswill Amadi <94016023+amadigodswill33@users.noreply.github.com>
2024-01-28 18:28:07 +0100

    Add files via upload

--- SQL_Data_Cleaning.sql
+++ SQL_Data_Cleaning.sql
@@ -1,50 +1,86 @@
 -- SQL DATA CLEANING PROJECT
 
--- Standardizing Sales Date
--- This way can have a standard date of '2013-08-28' instead of '2013-08-28 00:00:00.000'
-ALTER TABLE [Data Cleaning Project]..NashvilleHousing
+-- View the initial dataset
+SELECT *
+FROM [Data Cleaning Project]..Nashville;
+
+---------------------------------------------------------------------------------------------------------------------------------
+
+-- Standardizing Sales Date: Set a standard date of '2013-08-28' instead of '2013-08-28 00:00:00.000'
+ALTER TABLE [Data Cleaning Project]..Nashville
 ADD StandardSaleDate DATE;
 
-UPDATE [Data Cleaning Project]..NashvilleHousing
+-- Update the new column with standardized dates
+UPDATE [Data Cleaning Project]..Nashville
 SET StandardSaleDate = CONVERT(DATE, SaleDate);
 
--- Populate PropertyAddress
--- We will try to populate the PropertyAddress for missing addresses
+-- View the updated dataset
+SELECT *
+FROM [Data Cleaning Project]..Nashville;
+
+---------------------------------------------------------------------------------------------------------------------------------
+
+-- Populate PropertyAddress for missing addresses
 UPDATE a
 SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
-FROM [Data Cleaning Project]..NashvilleHousing AS a
-JOIN [Data Cleaning Project]..NashvilleHousing AS b
+FROM [Data Cleaning Project]..Nashville AS a
+JOIN [Data Cleaning Project]..Nashville AS b
 ON a.ParcelID = b.ParcelID
-AND a.[UniqueID] <> b.[UniqueID];
+AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL;
 
--- Breaking out Address into Individual Columns (i.e., Address, City, and States)
--- PROPERTYADDRESS
-ALTER TABLE [Data Cleaning Project]..NashvilleHousing
+-- View the updated dataset
+SELECT *
+FROM [Data Cleaning Project]..Nashville
+ORDER BY ParcelID;
+
+---------------------------------------------------------------------------------------------------------------------------------
+
+-- Splitting PropertyAddress into Address and City
+ALTER TABLE [Data Cleaning Project]..Nashville
 ADD PropertySplitAddress VARCHAR(255),
     PropertyCity VARCHAR(255);
 
-UPDATE [Data Cleaning Project]..NashvilleHousing
+UPDATE [Data Cleaning Project]..Nashville
 SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) - 1),
     PropertyCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1, LEN(PropertyAddress));
 
--- OWNERSADDRESS
-ALTER TABLE [Data Cleaning Project]..NashvilleHousing
+-- View the updated dataset
+SELECT *
+FROM [Data Cleaning Project]..Nashville;
+
+---------------------------------------------------------------------------------------------------------------------------------
+
+-- Splitting OwnersAddress into Address, City, and State
+ALTER TABLE [Data Cleaning Project]..Nashville
 ADD OwnersplitAddress VARCHAR(255),
     OwnersplitCity VARCHAR(255),
     OwnersplitState VARCHAR(255);
 
-UPDATE [Data Cleaning Project]..NashvilleHousing
+UPDATE [Data Cleaning Project]..Nashville
 SET OwnersplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3),
     OwnersplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2),
     OwnersplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1);
 
+-- View the updated dataset
+SELECT *
+FROM [Data Cleaning Project]..Nashville;
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
 -- Change Y and N to Yes and No in 'Sold as Vacant' field
-UPDATE [Data Cleaning Project]..NashvilleHousing
+UPDATE [Data Cleaning Project]..Nashville
 SET SoldAsVacant = CASE
                     WHEN SoldAsVacant = 'Y' THEN 'Yes'
                     WHEN SoldAsVacant = 'N' THEN 'No'
                     ELSE SoldAsVacant
                   END;
+
+-- View the updated dataset
+SELECT *
+FROM [Data Cleaning Project]..Nashville;
+
+-----------------------------------------------------------------------------------------------------------------------------------------
 
 -- Removing Duplicates
 WITH RownumCTE AS (
@@ -56,15 +92,23 @@ WITH RownumCTE AS (
                         LegalReference
            ORDER BY UniqueID
          ) AS row_num
-  FROM [Data Cleaning Project]..NashvilleHousing
+  FROM [Data Cleaning Project]..Nashville
 )
--- DELETE
-SELECT *
+-- Delete duplicates
+SELECT * 
 FROM RownumCTE;
 
--- Delete Columns
-ALTER TABLE [Data Cleaning Project]..NashvilleHousing
-DROP COLUMN OwnerAddress, PropertyAddress, TaxDistrict;
+------------------------------------------------------------------------------------------------------------------------------------------
 
-SELECT *
-FROM [Data Cleaning Project]..NashvilleHousing;
+-- Delete Columns
+ALTER TABLE [Data Cleaning Project]..Nashville
+DROP COLUMN SaleDate, OwnerAddress, PropertyAddress, TaxDistrict;
+
+-- Rename the column StandardSaleDate to SaleDate
+USE [Data Cleaning Project];
+EXEC sp_rename 'Nashville.StandardSaleDate', 'SaleDate', 'COLUMN';
+
+-- View the final dataset
+SELECT * 
+FROM [Data Cleaning Project]..Nashville
+ORDER BY UniqueID;
